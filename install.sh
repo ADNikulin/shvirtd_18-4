65794fbd2cb2837d1d176a0a11d4093b580a754b
anikulin <anikulin@ufs.travel>
2024-07-21 03:08:33 +0300

    fix

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ then
 	$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
 	sudo tee /etc/apt/sources.list.d/docker.list > /dev/null &&
 	sudo apt-get update &&
-	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin &&
+	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin
 fi
 
 if [ ! -d "/opt/shvirtd-example-python" ] ; then
